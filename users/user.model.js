190bcbfd9206a74be1cc7751d361ddc92c89ab86
Manideep519 <manideepyadav359@gmail.com>
2023-09-01 20:02:17 +0530

    test update

--- users/user.model.js
+++ users/user.model.js
@@ -1,14 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
-    username: { type: String, unique: true, required: true },
-    hash: { type: String, required: true },
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    createdDate: { type: Date, default: Date.now }
+  username: { type: String, unique: true, required: true },
+  hash: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  role: { type: String, required: true },
+  loginTime: { type: Date, required: false },
+  logoutTime: { type: Date, required: false },
+  clientIP: { type: String, required: false },
+  createdDate: { type: Date, default: Date.now },
 });
 
-schema.set('toJSON', { virtuals: true });
+schema.set("toJSON", { virtuals: true });
 
-module.exports = mongoose.model('User', schema);
+module.exports = mongoose.model("User", schema);
